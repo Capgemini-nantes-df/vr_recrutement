5af02305508cea09b6759ce7790268464c1ea665
rGoislot <39409519+rGoislot@users.noreply.github.com>
2018-06-25 17:15:44 +0200

    Add Unity Project new Environment

---
+++ Test Environment - Formation Réalité Virtuelle/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SelectPanel(GameObject panel)
+    {
+        GameObject parent = panel.transform.parent.gameObject;
+        int childs = parent.transform.childCount;
+        int i = 0;
+
+        panel.SetActive(true);
+        
+        while(i < childs)
+        {
+            if(parent.transform.GetChild(i).gameObject != panel)
+            {
+                parent.transform.GetChild(i).gameObject.SetActive(false);
+            }
+            i++;
+        }
+    }
+
+
+}
